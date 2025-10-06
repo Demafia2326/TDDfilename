a1c185ee20b384ec9297327327d225365adf1981
Demafia2326 <47661107+Demafia2326@users.noreply.github.com>
2025-10-06 17:01:53 +0200

    qwertyui

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	
+	
+	@Test
+	void testSuma() {
+		assertEquals(5, Calculadora.suma(2,3));
+	}
+	
+	@Test
+	void testResta() {
+		assertEquals(5, Calculadora.resta(3,2));
+	}
+	
+	@Test
+	void testMultiplica() {
+		assertEquals(5, Calculadora.multiplicacion(2,3));
+	}
+	
+	@Test
+	void testDivide() {
+		assertEquals(5, Calculadora.division(6,2));
+	}
+
+}
